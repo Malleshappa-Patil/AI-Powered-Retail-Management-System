8b784e4985511940ab32ac055d4bedb7040ec83b
Mahesh Patil <maheshpatil@Malleshappa.local>
2025-09-05 11:48:38 +0530

    second commit

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,6 +1,5 @@
-// src/pages/LoginPage.js
 import React, { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom'; // This line is corrected
+import { useNavigate, Link } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 import './LoginPage.css';
 
